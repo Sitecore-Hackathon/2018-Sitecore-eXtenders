9cb843f3cf63844534dad111d906a46e380e176a
Mohamed Krimi <mohamed.krimi@nexio.com>
2018-03-03 14:52:16 -0500

    Create Commerce Instagram Feature Add Instagram Product Images SXA Component (Static View version, not connected to API)

---
+++ src/Feature/Instagram/code/Scextender.Feature.Instagram/Repositories/IImagesListRepository.cs
@@ -0,0 +1,8 @@
+﻿using Sitecore.XA.Foundation.Mvc.Repositories.Base;
+
+namespace Scextender.Feature.Instagram.Repositories
+{
+    public interface IImagesListRepository : IModelRepository
+    {
+    }
+}
---
+++ src/Feature/Instagram/code/Scextender.Feature.Instagram/Services/ImageRepositoryConfigurator.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Scextender.Feature.Instagram.Controllers;
+using Scextender.Feature.Instagram.Repositories;
+using Sitecore.DependencyInjection;
+
+namespace Scextender.Feature.Instagram.Services
+{
+    public class ImageRepositoryConfigurator : IServicesConfigurator
+    {
+        public void Configure(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddTransient<IImagesListRepository, ImagesListRepository>();
+            serviceCollection.AddTransient<ImagesListController>();
+        }
+
+    }
+}
